5395d2fc7d431a460d8f3ed17e0977b32e49a540
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-18 13:49:28 -0500

    accessor methods

---
+++ 06_accessor_methods.rb
@@ -0,0 +1,25 @@
+class GoodDog
+  def initialize(name)
+    @name = name
+  end
+
+  def name                  # This was renamed from "get_name"
+    @name
+  end
+
+  def name=(n)              # This was renamed from "set_name="
+    @name = n
+  end
+
+  def speak
+    "#{@name} says arf!"
+  end
+end
+
+sparky = GoodDog.new("Sparky")
+puts sparky.speak
+puts sparky.name
+sparky.name = "Spartacus"
+puts sparky.name
+
+
